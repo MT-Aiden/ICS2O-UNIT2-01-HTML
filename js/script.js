ee9ccaafb0574222b48e868b6ac346349faf726f
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-03-22 00:51:09 +0000

    coding

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aiden McLeod
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
